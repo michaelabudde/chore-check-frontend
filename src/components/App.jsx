e11a52bbd4fee0e635f9cc69dba45ecdd2693b45
michaela budde <michaelabudde87@gmail.com>
2024-04-02 08:32:55 -0300

    working on footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,8 +21,8 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 }
