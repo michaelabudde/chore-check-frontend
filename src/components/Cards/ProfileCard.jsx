e6512df42a8b31f38d4088eaf968bcd1677d2bb1
michaela budde <michaelabudde87@gmail.com>
2024-04-05 13:02:30 -0300

    started profile card

--- src/components/Cards/ProfileCard.jsx
+++ src/components/Cards/ProfileCard.jsx
@@ -1,23 +1,55 @@
 import { React, useContext } from "react";
-import { CurrentUserContext } from "../../contexts/CurrentUserContext.jsx";
-import { AuthContext } from "../../contexts/AuthContext.jsx";
+import styled from "styled-components";
+import default_profile_icon from "../../images/greendefaultprofile.png";
 
+// import { CurrentUserContext } from "../../contexts/CurrentUserContext.jsx";
+// import { AuthContext } from "../../contexts/AuthContext.jsx";
+const Card = styled.div`
+  // border: 2px solid #9fbd84;
+  background: #d3dbf4;
+  border-radius: 30px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  width: 90vw;
+  display: flex;
+  flex-direction: row;
+  gap: 24px;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  gap: 24px;
+  position: relative;
+`;
+const MemberAvatar = styled.div`
+  width: 60px;
+  height: 60px;
+  text-decoration: none;
+  object-fit: cover;
+  border-radius: 50%;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+`;
+// import curent user from backend
+// const username = currentUser ? currentUser.name : "Terrence Tegegne";
+const username = "Dana Adisa";
 const ProfileCard = ({ item, onCardClick }) => {
-  const { currentUser } = useContext(CurrentUserContext);
-  const { isLoggedIn } = useContext(AuthContext);
+  // const { currentUser } = useContext(CurrentUserContext);
+  // const { isLoggedIn } = useContext(AuthContext);
 
   return (
-    <div className="card__element">
-      <div className="card__title-container">
-        <h2 className="card__name"> {item.name}</h2>
-      </div>
-      <img
-        src={item.imageUrl}
-        className="card__image"
-        onClick={() => onCardClick(item)}
-        alt={item.name}
-      />
-    </div>
+    <Card>
+      <MemberAvatar>
+        {" "}
+        <img
+          className="nav_avatar"
+          src={default_profile_icon} // default avatar
+          alt="default user avatar"
+        />
+      </MemberAvatar>
+      <h2>{username}</h2>
+    </Card>
   );
 };
 export default ProfileCard;
