c4e82f674bc2702b5ae414f5efaf4e8c537d4e9c
michaela budde <michaelabudde87@gmail.com>
2024-04-11 04:36:58 -0300

    removed comments

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,155 +9,95 @@ import ChoreList from "../Lists/ChoreList";
 import MemberList from "../Lists/MemberList";
 const Main = ({ openModal }) => {
   return (
+    /* // Signed in main */
+
     <main className="main">
       <div className="main__catch">
-        <h1 className="main__title">Need Some Help Managing Chores?</h1>
-
-        <h2 className="main__subtitle">
-          Digitally track chores in any household.
-        </h2>
-
-        <button className="main__button" onClick={() => openModal("signup")}>
-          Sign up here
+        <h1>Your Completed Chores This Week</h1>
+        <button className="main__button" onClick={() => openModal("logChore")}>
+          Log a Chore
         </button>
+        <div className="main__calendar">
+          <Calendar />
+        </div>
+        <h2> Your Chore List</h2>
+        <button className="main__button" onClick={() => openModal("addChore")}>
+          Add a Chore
+        </button>
+        <ChoreList />
 
-        <img src={cleaning_1} alt="cleaning image" className="main__image" />
+        <h2> Your Household Members</h2>
+        <button className="main__button" onClick={() => openModal("addMember")}>
+          Add a Member
+        </button>
+        <MemberList />
 
-        <p className="main__p">
-          Family homes and Co-ops can be hard to manage.
-        </p>
-
-        <h2 className="main__subtitle">
-          Chore Check is an easy solution to tracking chores
-        </h2>
-      </div>
-
-      <div className="main__how">
-        <h1 className="main__title">How it works</h1>
-
-        <p className="main__p">
-          Users can create a chore list and add their household members. Then
-          when a chore is completed it is signed in a calendar like this.
-        </p>
-        {/* calendar placeholder*/}
         <img src={cleaning_2} alt="cleaning image" className="main__image" />
       </div>
-
-      {/* always return about section */}
-      <div className="main__about">
-        <div className="main__about_image">
-          <img
-            src={author_image}
-            alt="author image"
-            className="main__about_image"
-          />
-        </div>
-        <div className="main__about_text">
-          <h1 className="main__title"> About the author</h1>
-          <p className="main__p">
-            I am Michaela Budde. I am a full-stack software engineer, graduating
-            from TripleTen Software Engineer program. I am passionate about
-            community building and justice for people and the environment. I
-            hope to use my technological skills towards positive solutions.{" "}
-          </p>
-          <p className="main__p">
-            Chore Check was created as a solution to a common area of conflict
-            in households. By having a digital tracker it is easier to ensure
-            fairness among members of the house.
-          </p>
-        </div>
-      </div>
     </main>
+    // <main className="main">
+    //   <div className="main__catch">
+    //     <h1 className="main__title">Need Some Help Managing Chores?</h1>
+
+    //     <h2 className="main__subtitle">
+    //       Digitally track chores in any household.
+    //     </h2>
+
+    //     <button className="main__button" onClick={() => openModal("signup")}>
+    //       Sign up here
+    //     </button>
+
+    //     <img src={cleaning_1} alt="cleaning image" className="main__image" />
+
+    //     <p className="main__p">
+    //       Family homes and Co-ops can be hard to manage.
+    //     </p>
+
+    //     <h2 className="main__subtitle">
+    //       Chore Check is an easy solution to tracking chores
+    //     </h2>
+    //   </div>
+
+    //   <div className="main__how">
+    //     <h1 className="main__title">How it works</h1>
+
+    //     <p className="main__p">
+    //       Users can create a chore list and add their household members.
+    //     </p>
+    //     <p className="main__p">
+    //       Then when a chore is completed it is signed in a calendar like this.
+    //     </p>
+    //     {/* calendar placeholder*/}
+    //     <img src={cleaning_2} alt="cleaning image" className="main__image" />
+    //   </div>
+
+    //   {/* always return about section */}
+    //   <div className="main__about">
+    //     <div className="main__about_image">
+    //       <img
+    //         src={author_image}
+    //         alt="author image"
+    //         className="main__about_image"
+    //       />
+    //     </div>
+    //     <div className="main__about_text">
+    //       <h1 className="main__title"> About the author</h1>
+    //       <p className="main__p">
+    //         I am Michaela Budde. I am a full-stack software engineer, graduating
+    //         from TripleTen Software Engineer program. I am passionate about
+    //         community building and justice for people and the environment. I
+    //         hope to use my technological skills towards positive solutions.{" "}
+    //       </p>
+    //       <p className="main__p">
+    //         Chore Check was created as a solution to a common area of conflict
+    //         in households. By having a digital tracker it is easier to ensure
+    //         fairness among members of the house.
+    //       </p>
+    //     </div>
+    //   </div>
+    // </main>
   );
 };
 export default Main;
 
-{
-  /* // Signed in main */
-}
-
-{
-  /* <main className="main">
-<div className="main__catch">
-  <h1>Your Completed Chores This Week</h1>
-  <button className="main__button" onClick={() => openModal("logChore")}>
-    Log a Chore
-  </button>
-  <div className="main__calendar">
-    <Calendar />
-  </div>
-  <h2> Your Chore List</h2>
-  <button className="main__button" onClick={() => openModal("addChore")}>
-    Add a Chore
-  </button>
-  <ChoreList />
-
-  <h2> Your Household Members</h2>
-  <button className="main__button" onClick={() => openModal("addMember")}>
-    Add a Member
-  </button>
-  <MemberList />
-
-  <img src={cleaning_2} alt="cleaning image" className="main__image" />
-</div> */
-}
-
-//   const signedOutMain = (
-//    {/* move main_catch and main_how here*/}
-//   <div className="main__catch">
-//     <h1>Need Some Help Managing Chores?</h1>
-//     <h2>Digitally track chores in any household.</h2>
-//     <button className="main__button">Sign up here</button>
-//     <img src={cleaning_1} alt="cleaning image" className="main__image" />
-//     <p>Family homes and Co-ops can be hard to manage.</p>
-//     <h2>Chore Check is an easy solution to tracking chores</h2>
-//   </div>
-//   <div className="main__how">
-//     <h1>How it works</h1>
-//     <p>
-//       Users can create a chore list and add their household members. Then
-//       when a chore is completed it is signed in a calendar like this.
-//     </p>
-//     {/* calendar placeholder*/}
-//     <img src={cleaning_2} alt="cleaning image" className="main__image" />
-//   </div>
-//   <div className="main__about">
-//     <div className="main__about_image">
-//       <img
-//         src={author_image}
-//         alt="author image"
-//         className="main__about_image"
-//       />
-//     </div>
-//   );
-// const signedInMain = (
-//   <div className="main">
-//     <div className="main__calendar"></div>
-//     <h2> Your Chore List</h2>
-//     <div className="main__list"></div>
-//     <h2> Your Household Members</h2>
-//     <div className="main__list"></div>
-//     <img src={cleaning_2} alt="cleaning image" className="main__image" />
-//   </div>
-// );
-
 // {/* {isSignedIn ? signedInMain : signedOutMain} */}
-
-// <main className="main">
-//   <div className="main__catch">
-//     <h1>Need Some Help Managing Chores?</h1>
-//     <h2>Digitally track chores in any household.</h2>
-//     <button className="main__button">Sign up here</button>
-//     <img src={cleaning_1} alt="cleaning image" className="main__image" />
-//     <p>Family homes and Co-ops can be hard to manage.</p>
-//     <h2>Chore Check is an easy solution to tracking chores</h2>
-//   </div>
-//   <div className="main__how">
-//     <h1>How it works</h1>
-//     <p>
-//       Users can create a chore list and add their household members. Then
-//       when a chore is completed it is signed in a calendar like this.
-//     </p>
-//     {/* calendar placeholder*/}
-//     <img src={cleaning_2} alt="cleaning image" className="main__image" />
-//   </div>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -16,6 +16,21 @@ const Navigation = ({ isOpen, openModal, toggleDropdown, onClose }) => {
   useEsc(() => isOpen && toggleDropdown());
 
   return (
+    /* {isSignedIn ? signedInHeader : signedOutHeader} */
+    // Signed out Header:
+    // <nav className="nav">
+    //   <div>
+    //     <button className="nav_button" onClick={() => openModal("signup")}>
+    //       Sign Up
+    //     </button>
+    //   </div>
+    //   <div>
+    //     <button className="nav_button" onClick={() => openModal("signin")}>
+    //       Log In
+    //     </button>
+    //   </div>
+    // </nav>
+    // Signed in header
     <nav className="nav">
       <div className="nav_signedin">
         <div className="nav_container">
@@ -58,161 +73,3 @@ const Navigation = ({ isOpen, openModal, toggleDropdown, onClose }) => {
   );
 };
 export default Navigation;
-// Signed out Header:
-//  <div>
-// <button className="nav_button" onClick={() => openModal("signup")}>
-//   Sign Up
-// </button>
-// <button className="nav_button" onClick={() => openModal("signin")}>
-//   Log In
-// </button>
-// </div>
-{
-  /*Signed In Header*/
-}
-{
-  /* <div className="nav_signedin">
-  <div className="nav_container">
-    <div className="nav_trigger" onClick={toggleDropdown}>
-      <img
-        className="nav_avatar"
-        src={default_profile_icon} // default avatar
-        alt="default user avatar"
-      />
-    </div>
-
-    <div
-      ref={dropdownRef}
-      className={`nav_dropdown ${open || isOpen ? "active" : "inactive"}`}
-    >
-      <div className="nav__overlay" onClick={toggleDropdown} />
-      <ul>
-        <h2 className="nav_username">User Name</h2>
-        <DropdownItem
-          text={"My Profile"}
-          to="/profile"
-          onClick={() => handleItemClick("")}
-        />
-        <DropdownItem
-          text={"Edit Profile"}
-          to="/profile"
-          onClick={() => handleItemClick("")}
-        />
-        <DropdownItem text={"sign Out"} onClick={() => handleItemClick("")} />
-      </ul>
-    </div>
-  </div>
-</div>; */
-}
-
-// const signedOutHeader = (
-//   <>
-//     <button
-//       className="nav_button"
-//       onClick={() => openModal("signup")}
-//     >
-//       Sign Up
-//     </button>
-//     <button
-//       className="nav_button"
-//       onClick={() => openModal("login")}
-//     >
-//       sign In
-//     </button>
-//   </>
-// );
-// const signedInHeader = (
-//   <nav className="nav">
-//     <div className="nav_container">
-//       <div>
-//         {currentUser.avatar ? (
-//           <img
-//             className="nav_avatar"
-//             src={currentUser.avatar}
-//             alt="user avatar"
-//           />
-//         ) : (
-//           <img
-//             className="nav_avatar"
-//             src={default_profile_icon} // default avatar
-//             alt="default user avatar"
-//           />
-//         )}
-//       </div>
-//       <div className="nav_dropdown">
-//         <div className="nav_username">
-//           {username ? username : "User Name"}
-//         </div>
-//         <DropdownItem text={"My Profile"} to="/profile" />
-//         <DropdownItem text={"Edit Profile"} to="/profile" />
-//         <DropdownItem text={"sign Out"} to="/profile" />
-//       </div>
-//     </div>
-//   </nav>
-// );
-
-{
-  /* {isSignedIn ? signedInHeader : signedOutHeader} */
-}
-{
-  /* Signed out header */
-}
-
-// const handleToggleMenu = () => {
-//   setOpen(!open);
-//   // toggleMenu(); // Toggle the main menu in App.js
-// };
-// useClickOutside(dropdownRef, () => setOpen(false)); // Use the custom hook
-
-// const handleNavBar = () => {
-//   setOpen(!open);
-//   toggleMenu(); // Toggle the main menu in App.js
-// };
-// const { currentUser } = useContext(CurrentUserContext);
-// const { isSignedIn } = useContext(AuthContext);
-
-// const username = currentUser.name;
-
-// const isAvatarSet = Boolean(currentUser.avatar);
-
-// useEffect(() => {
-//   if (!isOpen) {
-//     toggleDropdown(); // Close dropdown if it's open initially
-//   }
-// }, [isOpen, toggleDropdown]);
-
-// useEffect(() => {
-//   if (!isOpen) {
-//     toggleDropdown(); // Close dropdown if it's open initially
-//   }
-// }, [isOpen, toggleDropdown]);
-// const handleItemClick = () => {
-//   toggleDropdown();
-//   openModal();
-// };
-// for later;
-// add edit profile to dropdown
-{
-  /* <DropdownItem
-text={"Edit Profile"}
-to="/profile"
-onClick={() => handleItemClick("")}
-/> */
-}
-
-// function DropdownItem(props) {
-//   const handleClick = () => {
-//     props.onClick(); // Call the onClick function passed from parent (openModal)
-//     props.toggleDropdown(); // Call the function to close the dropdown
-//   };
-
-//   return (
-//     <li className="nav_dropdown_item" onClick={handleClick}>
-//       {props.to ? (
-//         <Link to={props.to}>{props.text}</Link>
-//       ) : (
-//         <span>{props.text}</span>
-//       )}
-//     </li>
-//   );
-// }
